5ab588577b41b244dda8406d67d217cbc838953a
Muhammad Ali <155935067+muhammadali2828@users.noreply.github.com>
2024-10-11 23:18:52 +0500

    Update minmaxalgorithm.py

--- minmaxalgorithm.py
+++ minmaxalgorithm.py
@@ -1,5 +1,10 @@
 import math
-
-scores = [3,5,2,9]
-levels = math.log(len(scores), 2)
-print(levels)
+def min_max(curDepth, nodeIndex,maxTurn, scores, targetDepth):
+    if curDepth == targetDepth:
+        return scores[nodeIndex]
+    if maxTurn:
+        return max(min_max(curDepth + 1, nodeIndex * 2, False, scores, targetDepth),min_max(curDepth + 1, nodeIndex * 2 + 1, False, scores, targetDepth))
+    else:
+        return min(min_max(curDepth + 1, nodeIndex * 2, True, scores, targetDepth),min_max(curDepth + 1, nodeIndex * 2 + 1, True, scores, targetDepth))
+scores = [2,5,6,7,1,-1,0,8]
+print(min_max(0, 0, True, scores, 3))
